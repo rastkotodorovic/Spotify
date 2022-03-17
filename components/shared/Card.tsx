618ced7d7b1c44eb9fef33e2227cb73d47fdadc7
rastkotodorovic <rale.todorovic@gmail.com>
2022-03-17 22:55:34 +0100

    Card fix

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -65,7 +65,7 @@ export default function Card({ playlist, href, isFollowed, index, setIsFollowed 
     return (
         <div className="bg-gray-100 w-60 shadow-md rounded p-2">
             <div className="group relative" onClick={() => router.push(`/${href}/${playlist?.id}`)}>
-                <img alt="Placeholder" className="block h-48 w-full rounded"  src={playlist?.images[0]?.url} />
+                <img alt="Placeholder" className=" rounded aspect-square"  src={playlist?.images[0]?.url} />
                 <div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 duration-700 transition justify-evenly">
                     {href !== 'playlist' ? (
                         <button
@@ -101,7 +101,25 @@ export default function Card({ playlist, href, isFollowed, index, setIsFollowed 
             </div>
             <div className="p-2">
                 <h3 className="text-gray-600 py-1 text-base justify-center">{playlist?.name}</h3>
-                <p className="text-gray-400 text-sm mt-1">By @{playlist?.owner?.display_name}</p>
+                {playlist?.followers?.total && (
+                    <p className="text-gray-400 text-sm mt-1">{playlist?.followers?.total} followers</p>
+                )}
+
+                {playlist?.owner?.display_name && (
+                    <p className="text-gray-400 text-sm mt-1">By @{playlist?.owner?.display_name}</p>
+                )}
+
+                {playlist?.artists && (
+                    <p className="text-gray-400 text-sm mt-1">
+                        {playlist?.artists.map((artist) => {
+                            return (
+                                <>
+                                    {`${artist.name} `}
+                                </>
+                            )
+                        })}
+                    </p>
+                )}
             </div>
         </div>
     )
